607f63261f643aa233a3fdea4b9ea9f04663a75a
Pontus Nagy <pontusnagy@gmail.com>
2017-08-16 14:29:04 +0200

    split up App into separate components, added eslintrc and nvmrc

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <AppHeader
+          welcome="Agero"
+          date={new Date(Date.now()).toLocaleString()}
+        />
+        <AppIntro />
       </div>
     );
   }
 }
+
+const AppHeader = props =>
+  <div className="App-header">
+    <img src={logo} className="App-logo" alt="logo" />
+    <h2>Welcome to {props.welcome},the current date is {props.date}</h2>
+  </div>;
+
+const AppIntro = () =>
+  <p className="App-intro">
+    To get started, edit <code>src/App.js</code> and save to reload.
+  </p>;
 
 export default App;
