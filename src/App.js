a5d5a619089389a205dc9523e5a12e6a3a23e2e5
Pontus Nagy <pontusnagy@gmail.com>
2017-09-01 10:41:59 +0200

    Added state to handle clock ticking

--- src/App.js
+++ src/App.js
@@ -3,28 +3,47 @@ import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  state = {
+    date: ""
+  };
+
+  componentDidMount() {
+    this.timer = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timer);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <AppHeader
-          welcome="Agero"
-          date={new Date(Date.now()).toLocaleString()}
-        />
+        <AppHeader welcome="Agero" date={this.state.date} />
         <AppIntro />
       </div>
     );
   }
 }
 
-const AppHeader = props =>
+const AppHeader = props => (
   <div className="App-header">
     <img src={logo} className="App-logo" alt="logo" />
-    <h2>Welcome to {props.welcome},the current date is {props.date}</h2>
-  </div>;
+    <h2>
+      Welcome to {props.welcome}, the current date is {props.date.toLocaleString()}
+    </h2>
+  </div>
+);
 
-const AppIntro = () =>
+const AppIntro = () => (
   <p className="App-intro">
     To get started, edit <code>src/App.js</code> and save to reload.
-  </p>;
+  </p>
+);
 
 export default App;
